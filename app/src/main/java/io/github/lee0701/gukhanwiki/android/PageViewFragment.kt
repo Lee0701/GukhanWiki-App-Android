95ec89f2cdda0c4abae844b4e29f83130c042e23
Lee0701 <hgl1002@naver.com>
2023-03-16 18:33:36 +0900

    Add basic paragraph parsing

--- app/src/main/java/io/github/lee0701/gukhanwiki/android/PageViewFragment.kt
+++ app/src/main/java/io/github/lee0701/gukhanwiki/android/PageViewFragment.kt
@@ -5,12 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Space
+import android.widget.TextView
+import androidx.appcompat.view.ContextThemeWrapper
+import androidx.appcompat.widget.LinearLayoutCompat
+import androidx.core.view.marginBottom
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
+import com.google.android.flexbox.FlexDirection
+import com.google.android.flexbox.FlexWrap
+import com.google.android.flexbox.FlexboxLayout
+import com.google.android.flexbox.JustifyContent
 import io.github.lee0701.gukhanwiki.android.databinding.FragmentPageViewBinding
 import io.github.lee0701.gukhanwiki.android.view.MainViewModel
 import io.github.lee0701.gukhanwiki.android.view.PageContent
 import io.github.lee0701.gukhanwiki.android.view.PageViewModel
+import org.jsoup.Jsoup
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -39,10 +49,49 @@ class PageViewFragment : Fragment() {
             activityViewModel.updateTitle(title)
         }
         viewModel.content.observe(viewLifecycleOwner) { content ->
+            val context = ContextThemeWrapper(context ?: return@observe, R.style.Theme_GukhanWikiAppAndroid_WikiPage)
             when(content) {
                 is PageContent.Loading -> {}
                 is PageContent.Loaded -> {
-                    binding.webView.loadData(content.text, "text/html", "UTF-8")
+                    binding.contentView.removeAllViews()
+                    val wrapContent = { FlexboxLayout.LayoutParams(
+                        FlexboxLayout.LayoutParams.WRAP_CONTENT,
+                        FlexboxLayout.LayoutParams.WRAP_CONTENT)
+                    }
+                    val doc = Jsoup.parse(content.text)
+                    val paragraphs = doc.select("h1, h2, h3, h4, h5, h6, p")
+                    paragraphs.map { paragraph ->
+                        val words = paragraph.text().split(Regex("\\s")).map { word ->
+                            TextView(context).apply {
+                                text = word
+                            }
+                        }
+                        FlexboxLayout(context).apply {
+                            flexDirection = FlexDirection.ROW
+                            flexWrap = FlexWrap.WRAP
+                            justifyContent = JustifyContent.SPACE_BETWEEN
+                            layoutParams = LinearLayoutCompat.LayoutParams(
+                                LinearLayoutCompat.LayoutParams.MATCH_PARENT,
+                                LinearLayoutCompat.LayoutParams.WRAP_CONTENT
+                            ).apply {
+                                bottomMargin = resources.getDimension(R.dimen.line_spacing).toInt()
+                            }
+                            words.forEach { word ->
+                                addView(word.apply {
+                                    layoutParams = wrapContent().apply {
+                                        rightMargin = resources.getDimension(R.dimen.word_spacing).toInt()/2
+                                        leftMargin = resources.getDimension(R.dimen.word_spacing).toInt()/2
+                                    }
+                                })
+                            }
+                            addView(Space(context).apply {
+                                layoutParams = wrapContent().apply { flexGrow = 1.0f }
+                            })
+                        }
+                    }.forEach { paragraph ->
+                        binding.contentView.addView(paragraph)
+                    }
+//                    binding.webView.loadData(content.text, "text/html", "UTF-8")
                 }
                 is PageContent.Error -> {}
             }
