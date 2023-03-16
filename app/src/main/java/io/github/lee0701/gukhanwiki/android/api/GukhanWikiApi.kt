3cd1165aa28253936582fa5194a7d4e70ed57f17
Lee0701 <hgl1002@naver.com>
2023-03-16 17:17:06 +0900

    Add development api host build config

--- app/src/main/java/io/github/lee0701/gukhanwiki/android/api/GukhanWikiApi.kt
+++ app/src/main/java/io/github/lee0701/gukhanwiki/android/api/GukhanWikiApi.kt
@@ -1,16 +1,18 @@
 package io.github.lee0701.gukhanwiki.android.api
 
+import io.github.lee0701.gukhanwiki.android.BuildConfig
 import retrofit2.Retrofit
 import retrofit2.converter.scalars.ScalarsConverterFactory
+import java.net.URL
 
 object GukhanWikiApi {
-    const val HOST = "wiki.xn--9cs231j0ji.xn--p8s937b.net"
-    const val BASE_URL = "https://$HOST/"
-    const val BASE_PATH = "rest.php/v1/"
+    const val PROTOCOL = "https"
+    const val HOST = BuildConfig.API_HOST
+    const val BASE_PATH = "/rest.php/v1/"
 
     private val retrofit: Retrofit by lazy {
         Retrofit.Builder()
-            .baseUrl("$BASE_URL$BASE_PATH")
+            .baseUrl(URL(PROTOCOL, HOST, BASE_PATH))
             .addConverterFactory(ScalarsConverterFactory.create())
             .build()
     }
