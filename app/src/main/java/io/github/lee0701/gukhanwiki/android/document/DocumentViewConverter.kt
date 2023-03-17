8afd92f347fc2dd3b8cf16f9bb0536a6e23d8e92
Lee0701 <hgl1002@naver.com>
2023-03-17 17:19:45 +0900

    Add basic document to view converter

---
+++ app/src/main/java/io/github/lee0701/gukhanwiki/android/document/DocumentViewConverter.kt
@@ -0,0 +1,128 @@
+package io.github.lee0701.gukhanwiki.android.document
+
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Space
+import android.widget.TextView
+import androidx.annotation.StyleRes
+import androidx.appcompat.view.ContextThemeWrapper
+import androidx.appcompat.widget.LinearLayoutCompat
+import com.google.android.flexbox.FlexDirection
+import com.google.android.flexbox.FlexWrap
+import com.google.android.flexbox.FlexboxLayout
+import com.google.android.flexbox.JustifyContent
+import io.github.lee0701.gukhanwiki.android.R
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Element
+import org.jsoup.nodes.TextNode
+
+class DocumentViewConverter(
+    context: Context,
+) {
+    val context = ContextThemeWrapper(context, R.style.Theme_GukhanWikiAppAndroid_WikiPage)
+
+    fun convert(html: String): View? {
+        val doc = Jsoup.parse(html)
+        return parseRecursive(context, doc.body())
+    }
+
+    fun parseRecursive(context: Context, element: Element): View? {
+        val view = when(element.tagName()) {
+            "section" -> sectionElement(context)
+            in TAGS_HEADER -> headerElement(context, TAGS_HEADER.indexOf(element.tagName()))
+            "p" -> paragraphElement(context)
+            "a" -> anchorElement(context)
+            "span" -> spanElement(context)
+            "body" -> bodyElement(context)
+            else -> null
+        }
+        val newContext = view?.context ?: return null
+        val children = element.childNodes().mapNotNull { node ->
+            when(node) {
+                is TextNode -> textNode(newContext, node.text())
+                is Element -> parseRecursive(newContext, node)
+                else -> null
+            }
+        }
+        if(view is ViewGroup) {
+            children.forEach { view.addView(it) }
+            if(element.tagName() == "p") {
+                view.addView(Space(context).apply {
+                    layoutParams = FlexboxLayout.LayoutParams(
+                        FlexboxLayout.LayoutParams.WRAP_CONTENT,
+                        FlexboxLayout.LayoutParams.WRAP_CONTENT,
+                    ).apply {
+                        flexGrow = 1.0f
+                    }
+                })
+            }
+        }
+        return view
+    }
+
+    private fun sectionElement(context: Context): View {
+        return LinearLayoutCompat(context).apply {
+            orientation = LinearLayoutCompat.VERTICAL
+        }
+    }
+
+    private fun headerElement(baseContext: Context, level: Int): View {
+        val context = ContextThemeWrapper(baseContext, HEADER_TEXT_STYLES[level])
+        return LinearLayoutCompat(context).apply {
+            orientation = LinearLayoutCompat.HORIZONTAL
+        }
+    }
+
+    private fun paragraphElement(context: Context): View {
+        return FlexboxLayout(context).apply {
+            flexDirection = FlexDirection.ROW
+            flexWrap = FlexWrap.WRAP
+            justifyContent = JustifyContent.SPACE_BETWEEN
+            layoutParams = LinearLayoutCompat.LayoutParams(
+                LinearLayoutCompat.LayoutParams.MATCH_PARENT,
+                LinearLayoutCompat.LayoutParams.WRAP_CONTENT
+            ).apply {
+                bottomMargin = resources.getDimension(R.dimen.line_spacing).toInt()
+            }
+        }
+    }
+
+    private fun anchorElement(context: Context): View {
+        return LinearLayoutCompat(context).apply {
+            orientation = LinearLayoutCompat.HORIZONTAL
+            isClickable = true
+        }
+    }
+
+    private fun spanElement(context: Context): View {
+        return LinearLayoutCompat(context).apply {
+            orientation = LinearLayoutCompat.HORIZONTAL
+            isClickable = true
+        }
+    }
+
+    private fun bodyElement(context: Context): View {
+        return LinearLayoutCompat(context).apply {
+            orientation = LinearLayoutCompat.VERTICAL
+        }
+    }
+
+    private fun textNode(context: Context, text: String): View {
+        return TextView(context).apply {
+            this.text = text
+        }
+    }
+
+    companion object {
+        val TAGS_HEADER = listOf("h1", "h2", "h3", "h4", "h5", "h6")
+        @StyleRes val HEADER_TEXT_STYLES = listOf(
+            R.style.Theme_GukhanWikiAppAndroid_WikiPage_H1,
+            R.style.Theme_GukhanWikiAppAndroid_WikiPage_H2,
+            R.style.Theme_GukhanWikiAppAndroid_WikiPage_H3,
+            R.style.Theme_GukhanWikiAppAndroid_WikiPage_H4,
+            R.style.Theme_GukhanWikiAppAndroid_WikiPage_H5,
+            R.style.Theme_GukhanWikiAppAndroid_WikiPage_H6,
+        )
+    }
+}
