949b4e5bd23a2ce6cad95d04f4633675fe5c1211
Lee0701 <hgl1002@naver.com>
2023-03-26 00:05:29 +0900

    Add footnote popup

--- app/src/main/java/io/github/lee0701/gukhanwiki/android/view/WebViewRenderer.kt
+++ app/src/main/java/io/github/lee0701/gukhanwiki/android/view/WebViewRenderer.kt
@@ -1,5 +1,7 @@
 package io.github.lee0701.gukhanwiki.android.view
 
+import org.jsoup.nodes.Document
+
 interface WebViewRenderer {
-    fun render(content: String): String
+    fun render(content: String): Document
 }
