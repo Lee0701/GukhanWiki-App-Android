372d5ba04030c58bfab873ad45529498c5833efe
Lee0701 <hgl1002@naver.com>
2023-04-18 21:51:16 +0900

    Add ko-Kore-KR locale and switching mechanism

--- app/src/main/java/io/github/lee0701/gukhanwiki/android/GukhanWikiApplication.kt
+++ app/src/main/java/io/github/lee0701/gukhanwiki/android/GukhanWikiApplication.kt
@@ -1,11 +1,19 @@
 package io.github.lee0701.gukhanwiki.android
 
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.os.LocaleListCompat
 import androidx.multidex.MultiDexApplication
+import androidx.preference.PreferenceManager
 import io.github.lee0701.gukhanwiki.android.auth.AccountHelper
 
 class GukhanWikiApplication: MultiDexApplication() {
     override fun onCreate() {
         super.onCreate()
+        val preference = PreferenceManager.getDefaultSharedPreferences(this)
         AccountHelper.initAccountManager(this)
+
+        val localeCode = preference.getString("display_locale", "ko-Kore-KR")
+        val appLocale: LocaleListCompat = LocaleListCompat.forLanguageTags(localeCode)
+        AppCompatDelegate.setApplicationLocales(appLocale)
     }
 }
--- app/src/main/java/io/github/lee0701/gukhanwiki/android/SettingsActivity.kt
+++ app/src/main/java/io/github/lee0701/gukhanwiki/android/SettingsActivity.kt
@@ -1,12 +1,19 @@
 package io.github.lee0701.gukhanwiki.android
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.content.SharedPreferences
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.PreferenceManager
 
-class SettingsActivity : AppCompatActivity() {
+class SettingsActivity: AppCompatActivity(), OnSharedPreferenceChangeListener {
+
+    private val preference: SharedPreferences by lazy { PreferenceManager.getDefaultSharedPreferences(this) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +24,7 @@ class SettingsActivity : AppCompatActivity() {
             .commit()
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         PreferenceManager.setDefaultValues(this, R.xml.root_preferences, false)
+        preference.registerOnSharedPreferenceChangeListener(this)
     }
 
     class SettingsFragment : PreferenceFragmentCompat() {
@@ -30,5 +38,30 @@ class SettingsActivity : AppCompatActivity() {
             finish()
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    @SuppressLint("ApplySharedPref")
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
+        val displayLocaleKey = "display_locale"
+        if(key == displayLocaleKey) {
+            if(sharedPreferences != null) {
+                val value = sharedPreferences.getString(key, "ko-Kore-KR")
+                sharedPreferences.edit().putString(displayLocaleKey, value).commit()
+            }
+            restartApp()
+        }
+    }
+
+    private fun restartApp() {
+        val intent = applicationContext.packageManager.getLaunchIntentForPackage(applicationContext.packageName)
+        val mainIntent = Intent.makeRestartActivityTask(intent?.component)
+        applicationContext.startActivity(mainIntent)
+        finishAffinity()
+        Runtime.getRuntime().exit(0)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        preference.unregisterOnSharedPreferenceChangeListener(this)
     }
 }
