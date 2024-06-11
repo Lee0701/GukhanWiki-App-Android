14b7396d88db0be9efb21365d2ed2b3a3796f765
Lee0701 <hgl1002@naver.com>
2024-06-11 12:51:48 +0900

    Re-enable code minification with uploading mapping file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         applicationId = "io.github.lee0701.gukhanwiki.android"
         minSdk = 21
         targetSdk = 34
-        versionCode = 23
+        versionCode = 24
         versionName = "0.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
@@ -58,8 +58,8 @@ android {
     }
     buildTypes {
         release {
-            isMinifyEnabled = false
-            isShrinkResources = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
